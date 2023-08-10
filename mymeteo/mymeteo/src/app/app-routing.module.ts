da6096b6d8133bb15fac07c3f912f03cff51a02a
Daviduxx <borotto.davide@gmail.com>
2023-08-10 23:26:06 +0200

    router

--- mymeteo/mymeteo/src/app/app-routing.module.ts
+++ mymeteo/mymeteo/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { IntroComponent } from './components/intro/intro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IntroComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
