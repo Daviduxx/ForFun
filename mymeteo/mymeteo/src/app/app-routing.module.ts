2da84194e47804d445d2b2facd518a6b022acadd
Daviduxx <borotto.davide@gmail.com>
2023-08-12 22:19:56 +0200

    routing

--- mymeteo/mymeteo/src/app/app-routing.module.ts
+++ mymeteo/mymeteo/src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: IntroComponent
+    component: HomeComponent
   },
   {
-    path: 'home',
-    component: HomeComponent
-  }
+    path: 'weather',
+    component: WeatherComponent
+  },
+  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }
 ];
 
 @NgModule({
--- mymeteo/mymeteo/src/app/app.module.ts
+++ mymeteo/mymeteo/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { WeatherComponent } from './components/weather/weather.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IntroComponent
+    IntroComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
