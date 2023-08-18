ea985d549da009deb06ca2e4ebea6fac57f762ea
Daviduxx <borotto.davide@gmail.com>
2023-08-19 00:09:20 +0200

    environment

---
+++ mymeteo/mymeteo/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  WEATHER_API: 'https://api.open-meteo.com/v1/forecast?latitude=45.0705&longitude=7.6868&hourly=temperature_2m,precipitation_probability,visibility,is_day&daily=weathercode,sunrise,sunset&timezone=auto'
+};
