f09ae7ce5c3dced0039c5ec5670fa6f41246adc4
Daviduxx <borotto.davide@gmail.com>
2024-04-03 10:36:02 +0200

    buttons

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -2,6 +2,7 @@ let turn = true;
 let victory = false;
 let count = 0;
 let res = ['', '', '', '', '', '', '', '', ''];
+let playing = false;
 let winningConsitions = [
     [0,1,2],
     [3,4,5],
@@ -14,11 +15,33 @@ let winningConsitions = [
 ]
 let player = document.querySelector('#player');
 const tiles = Array.from(document.querySelectorAll('.tile'))
+let playButton = document.querySelector('#play');
+let minuti = 0;
+let secondi = 0;
+
+function chronometer(){
+    timer = setInterval(updateTime, 1000)
+}
+
+function updateTime(){
+     secondi++;
+        if(secondi == 60){
+            secondi = 0;
+            minuti++;
+        }
+    document.getElementById('chronometer').innerText = pad(minuti) + ':' + pad(secondi);
+}
+
+
+
+playButton.addEventListener('click', () => play())
+
 
 tiles.forEach((t,i) => {
    t.addEventListener('click', () => {
-        if(t.innerHTML == '' && !victory){
+        if(t.innerHTML == '' && !victory && playing){
             turn ? t.innerHTML = 'X' : t.innerHTML = 'O';
+            turn ? player.innerHTML = 'O ' : player.innerHTML = 'X ';
             turn ? res[i] = 'X': res[i] = 'O'
             count++;   
             checkRes(res)
@@ -35,7 +58,6 @@ function checkRes(arr){
         for(let j = 0; j < 3; j++){
             if(arr[winningConsitions[i][j]] == provRes){
                 counter++;
-                console.log("più uno " + counter)
             } 
         }
         if(counter == 3){
@@ -43,5 +65,18 @@ function checkRes(arr){
             break;
         }
     }
+}
+
+
+
+function pad(valore) {
+  return valore < 10 ? '0' + valore : valore;
+}
+
+function play(){
+    playing = true;
+    player.parentElement.classList.remove('hide');
+    playButton.classList.add('hide')
+    chronometer();
 }
 
