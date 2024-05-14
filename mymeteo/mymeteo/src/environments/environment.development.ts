77ac73a5e03f919a17332a1a4e894365baf081f8
Daviduxx <borotto.davide@gmail.com>
2024-05-14 21:00:38 +0200

    clean up

--- mymeteo/mymeteo/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  WEATHER_API: 'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,precipitation_probability,visibility,is_day&daily=weathercode,sunrise,sunset&timezone=auto&latitude=45.0705&longitude=7.6868',
-  GEOCODING_API: 'https://geocoding-api.open-meteo.com/v1/search?name=Sydney&count=1&language=en&format=json'
-};
